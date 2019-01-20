f51e14ea61825e4920eefc41cd91548319e3068b
Yasas <wayasas@gmail.com>
2019-01-20 12:22:20 +0530

    Add download tools script

--- scripts/run_examples.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-python -m tweetkit.examples.parser <path_to_tool_folder>
-cd scripts
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='tweetkit',
+    version='0.1',
+    packages=['tweetkit'],
+    description='tweetkit library',
+    install_requires=[],
+)
