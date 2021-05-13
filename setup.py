731089fb68ffe951abca9e1becb688a2cf92dfa4
Yasas <ysenarath.93@gmail.com>
2021-05-13 09:43:45 -0400

    Reset project - Remove all Windows dependent source

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tweetkit',
-    version='0.1.1',
+    version='0.2.0-pre',
     packages=['tweetkit'],
     description='tweetkit library',
     install_requires=[],
