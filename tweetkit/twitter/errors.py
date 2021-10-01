bf14116e2808613135b40e88429e7010c9fcf50b
Yasas <ysenarath.93@gmail.com>
2021-09-30 23:02:33 -0400

    Add error message attribute

--- tweetkit/twitter/errors.py
+++ tweetkit/twitter/errors.py
@@ -8,4 +8,8 @@ class TwitterException(Exception):
         super(TwitterException, self).__init__(message)
         if errors is None:
             errors = []
+        self.message = message
         self.errors = errors
+
+    def __repr__(self):
+        return self.message
