3f549c29842ef951916140591517722d3b77e7d3
Yasas <ysenarath.93@gmail.com>
2021-10-29 00:01:26 -0400

    Update version to v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='tweetkit',
-    version='0.0.3',
+    version='0.0.4',
     packages=setuptools.find_packages(),
     description='TweetKit library',
     install_requires=[],
