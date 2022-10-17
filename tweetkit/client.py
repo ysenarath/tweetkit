af2f5b539e8b4587693f201d4132472d1763bbdd
Yasas <12231659+ysenarath@users.noreply.github.com>
2022-10-16 20:48:46 -0400

    Update to send requests with TwitterRequest object.

--- tweetkit/client.py
+++ tweetkit/client.py
@@ -1,10 +1,5 @@
 """Twitter API v2"""
-from __future__ import absolute_import
-
-import requests
-
-from tweetkit.exceptions import ProblemOrError, TwitterRequestException
-from tweetkit.models import TwitterResponse, TwitterStreamResponse
+from tweetkit.models import TwitterRequest
 from tweetkit.requests import Bookmarks, Compliance, General, Lists, Spaces, Tweets, Users
 
 
@@ -33,40 +28,12 @@ class TwitterClient(object):
         self.tweets = Tweets(self)
         self.users = Users(self)
 
-    def request(self, path, method='get', query=None, params=None, data=None, stream=False, **kwargs):
-        """Create a request object and return.
-
-        Parameters
-        ----------
-        path: str
-        method: str
-        query: dict, optional
-        params: dict, optional
-        data: dict, optional
-        stream: bool, optional
-        response_kwargs
-
-        Returns
-        -------
-        request: Request
-            Request object.
-        """
-        formatted_path = path.format(**params)
-        url = '{}/{}'.format(self.url.rstrip('/'), formatted_path.lstrip('/'))
-        query = {k: ','.join(v) if isinstance(v, list) else v for k, v in query.items()}
-        r = requests.request(method.upper(), url, params=query, json=data, stream=stream, auth=self.auth)
-        content_type = r.headers.get('content-type')
-        if 200 <= r.status_code < 300:
-            # The request has succeeded.
-            if content_type is None and stream:
-                return TwitterStreamResponse(r, **kwargs)
-            elif content_type is not None and content_type.startswith('application/json'):
-                return TwitterResponse(r, **kwargs)
-        else:
-            error = None
-            if content_type is not None and content_type.startswith(('application/json', 'application/problem+json')):
-                # The request has failed.
-                error = ProblemOrError(r)
-            if error is not None:
-                raise error
-        raise TwitterRequestException(r)
+    def request(self, url, method='get', query=None, params=None, data=None, stream=False, paginate=False,
+                **kwargs):
+        """make request and get response."""
+        url = '{}/{}'.format(self.url, url.lstrip('/'))
+        request = TwitterRequest(
+            url, method=method, query=query, params=params, data=data,
+            stream=stream, auth=self.auth, **kwargs
+        )
+        return request.send(paginate=paginate)
